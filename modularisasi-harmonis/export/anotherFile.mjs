6de93bdd2bc5ce0fc88e838c60c67794e10cfac6
Christian <christianluisginting@gmail.com>
2025-09-08 22:42:09 +0700

    export

---
+++ modularisasi-harmonis/export/anotherFile.mjs
@@ -0,0 +1,11 @@
+// export default function goodMorning() {
+//   console.log("Good Morning");
+// }
+
+export default function sayHi() {
+  console.log("Hai, ini default export dari anotherfile.mjs");
+}
+
+export function sayGoodBye() {
+  console.log("Bye, ini named export dari anotherfile.mjs");
+}
---
+++ modularisasi-harmonis/export/main.mjs
@@ -0,0 +1,8 @@
+// import goodMorning from "./anotherFile.mjs";
+import anotherName from "./anotherFile.mjs";
+import sayHi, { sayGoodBye } from "./anotherFile.mjs";
+
+// goodMorning();
+// anotherName();
+sayHi();
+sayGoodBye();
