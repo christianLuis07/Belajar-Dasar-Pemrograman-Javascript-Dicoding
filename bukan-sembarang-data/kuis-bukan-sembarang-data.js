3a2e49e053cc4f3e6256bc77c7531851b80395c4
Christian <christianluisginting@gmail.com>
2025-09-08 19:44:03 +0700

    final

---
+++ bukan-sembarang-data/kuis-bukan-sembarang-data.js
@@ -0,0 +1,19 @@
+const employees = [
+  {
+    name: "Fulan",
+    email: "fulan@dicoding.com",
+    joinYear: 2020,
+  },
+];
+
+function addEmployee(name, email, joinYear) {
+  /**
+   * @TODO
+   * lengkapi fungsi ini agar dapat menambahkan objek employee baru
+   * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
+   */
+  employees.push({ name, email, joinYear });
+}
+
+addEmployee("Someone", "someone@me.com", 2021);
+console.log(employees);
